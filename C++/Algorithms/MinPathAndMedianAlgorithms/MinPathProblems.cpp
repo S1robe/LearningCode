bb5b3ee3fef9da2ed67df97fd0957fd70e9e11b3
S1robe <avvakira@protonmail.ch>
2022-11-20 06:43:30 -0800

    fix #2

---
+++ C++/Algorithms/MinPathAndMedianAlgorithms/MinPathProblems.cpp
@@ -0,0 +1,35 @@
+//
+// Created by owner on 10/23/22.
+//
+
+#include "Graph.h"
+
+/**
+ * Givess min spanning tree
+ */
+void kruskellsAlgorithm(){}
+
+/**
+ * Gives min spanning tree
+ */
+void primsAlgorithm(){}
+
+/**
+ * weighted directed graphs
+ * Tells us the shortest distance from one node to every other node
+ */
+void dijkstras(Graph g){
+
+
+    for(int i = 0; i < g.size(); i++){
+        g.
+    }
+}
+
+void bellmanFord(){}
+
+void floydWarshall(){}
+
+void johnsons(){}
+
+void Astar(){}
