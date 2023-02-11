e99194edc5addaba19056848b160b662369c4aa3
Garrett Prentice <78630000+S1robe@users.noreply.github.com>
2023-02-11 08:37:31 -0800

    Add files via upload

--- C/Amicable/amNums.c
+++ C/Amicable/amNums.c
@@ -4,16 +4,11 @@
 #include "string.h"
 #include <limits.h>
 #include <math.h>
-
-/*
-struct {
-  int threads;
-  unsigned long limit;
-} args;*/
+#include <strings.h>
 
 pthread_mutex_t lock;
 
-unsigned long* arr;
+unsigned long (*arr)[2];
 
 unsigned long limit = 0;
 
@@ -67,11 +62,12 @@ void* amicable(void* arg){
     if((amiNum < limit) && (sumPropDiv(amiNum) == myNum)){
       pthread_mutex_lock(&lock);
       amiNums++; // for duplicates this will be fixed later
+
       if(idx == idxLimit)
-        arr = reallocarray(arr, (idxLimit = idxLimit + 2), sizeof(int)); // Extend array for new placement
-      arr[idx] = myNum; // place number
-      idx++;
-      arr[idx] = amiNum;
+        arr = reallocarray(arr, (idxLimit = idxLimit + 1)*2, sizeof(unsigned long)); // Extend array for new placement
+
+      arr[idx][0] = myNum; // place number
+      arr[idx][1] = amiNum;
       idx++;
       pthread_mutex_unlock(&lock);
     }
@@ -132,20 +128,77 @@ int validate(int argc, char** argv){
   return 1;
 }
 
+//Some how unecessary?
 //Removes dupilcates of sorted list, T(n)
-int remdup(int size){
-  int x = 0;
-  for (int i = 1; i < size; i ++) {
-    if (arr[x] != arr[i]) {
-      arr[++x] = arr[i];
+//int remdup(int size){
+//  int x = 0;
+//  for (int i = 1; i < size; i++) {
+//    if (arr[x][0] != arr[i][0]) {
+//      arr[++x][0] = arr[i][0];
+//      arr[x][1] = arr[i][1];
+//    }
+//  }
+//  return x+1; // return new size
+//}
+
+void merge(int l, int m, int r)
+{
+    int i, j, k,
+    nd1 = m - l + 1,
+    nd2 = r - m;
+
+    unsigned long L[nd1][2], R[nd2][2];
+
+    for (i = 0; i < nd1; i++){
+        L[i][0] = arr[l + i][0];
+        L[i][1] = arr[l + i][1];
     }
-  }
-  return x+1; // return new size
+    for (j = 0; j < nd2; j++){
+        R[j][0] = arr[m + 1 + j][0];
+        R[j][1] = arr[m + 1 + j][1];
+    }
+
+    i = j = 0;
+    k = l;
+    while (i < nd1 && j < nd2) {
+        if (L[i][1] <= R[j][1]) { // take left while l <= r
+            arr[k][0] = L[i][0];
+            arr[k][1] = L[i][1];
+            i++;
+        }
+        else {
+            arr[k][0] = R[j][0];
+            arr[k][1] = R[j][1];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < nd1) { // take rest of L
+        arr[k][0] = L[i][0];
+        arr[k][1] = L[i][1];
+        i++;
+        k++;
+    }
+
+    while (j < nd2) { // take rest of R
+        arr[k][0] = R[j][0];
+        arr[k][1] = R[j][1];
+        j++;
+        k++;
+    }
 }
 
-int cmp(const void * a, const void * b){
-  return *((int*) a)  == *((int*)b) ? 0 : *((int*)a) > *((int*)b) ? 1 : -1;
+void ms(int l, int r)
+{
+    if (l < r) {
+        int m = (l+r) / 2;
+        ms(l, m);  // break left
+        ms(m + 1, r); // break right
+        merge(l, m, r); // merge lists
+    }
 }
+
 
 int main(int argc, char ** argv){
 
@@ -156,9 +209,17 @@ int main(int argc, char ** argv){
 
   int tCnt = atoi(argv[2]); // threadCnt
 
-  idxLimit = limit/2;       // arbitrary initial index limit because using realloc later, trying to cut down on that being called as much!
 
-  arr = (unsigned long *) malloc(sizeof(unsigned long)*(idxLimit)); // make dynamic array
+  // Have to check this otherwise malloc will fail because it will be a weird array and just bad...
+  if(limit %256 == 0)
+    idxLimit = limit/256;       // arbitrary initial index limit because using realloc later, trying to cut down on that being called as muc
+  else
+    idxLimit = (limit+(256-limit%256))/256;     // should be fine
+
+
+  arr = malloc(idxLimit * sizeof(*arr)); // initialize 2 rows each with idxLimit*sizeof(unsigned long) for a full 2d array of idxLimit*2
+
+  //  arr = (unsigned long ) malloc(sizeof(unsigned long)*(idxLimit)); // make dynamic array
 
   pthread_t thrds[tCnt];    // make baby thread
 
@@ -172,8 +233,6 @@ int main(int argc, char ** argv){
   for(int i = 0; i < tCnt; i++)
     pthread_join(thrds[i], NULL);
 
-  amiNums = remdup(idxLimit) -2; // fix from index
-
 
   // Yay formatting
   printf("CS 370 - Project #3\n%s\n\n%s%d\n%s%d\n%s%s\n\n%s\n\n%s\n",
@@ -184,16 +243,15 @@ int main(int argc, char ** argv){
           "Please wait. Running ...",
           "Amicable Numbers");
 
+  //sort
+  ms(0, amiNums);
 
-  for(int i = amiNums;  i >= 0; i--){
-    printf("%7lu", arr[i]);
-    i--;
-    printf("%7lu\n", arr[i]);
+  for(int i = 1;  i < amiNums; i++){
+    printf("%7lu%7lu\n", arr[i][1], arr[i][0]);
   }
 
-  amiNums = (amiNums+1)/2; // fix from index and divide to become pairs
-
+  free(arr);
   printf("\nCount of amicable number pairs from 1 to %s is %d\n", argv[4],  amiNums );
 }
 
-
+// need to write a sorting function that sorts upper "parallel array" and then applies result simultaneously to the mirror
