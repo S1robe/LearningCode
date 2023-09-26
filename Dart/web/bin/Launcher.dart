053e0e303d6202afdae78dec6b66c91129511dc5
S1robe <garrettprentice@tuta.io>
2023-09-25 18:01:03 -0700

    adding dart

---
+++ Dart/web/bin/Launcher.dart
@@ -0,0 +1,25 @@
+//I am the main I will launch the game.
+
+class Launcher {
+  bool gameisRunning;
+  Game game;
+
+  Launcher(){
+    
+  }
+
+  void start(){
+
+  }
+
+  void stop(){
+
+  }
+
+}
+
+void main(List<String> args){
+  
+}
+
+
